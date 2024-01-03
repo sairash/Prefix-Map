5a7935b6e1448f07d8924d9d3ed9d5518629ea6f
sairash <gautamsairash@gmail.com>
2024-01-03 23:58:48 +0545

    new and exciting changes

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module prefix_map
 
 go 1.21.5
+
+require github.com/dolthub/swiss v0.2.1
+
+require github.com/dolthub/maphash v0.1.0 // indirect
--- main.go
+++ main.go
@@ -2,199 +2,128 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"strings"
+	"sync"
+
+	"github.com/dolthub/swiss"
 )
 
-type NodeValues struct {
-	Value string
-	Done  chan struct{}
+type Segmenter func(k string, init int) (ret string, next int)
+
+type Segnode[v any] struct {
+	Mu       sync.RWMutex
+	Value    []v
+	Children *swiss.Map[string, *Segnode[v]]
 }
 
-type Node struct {
-	Values     []**NodeValues
-	AutoDelete bool
-	Children   [27]*Node
+type Segmap[v any] struct {
+	Node      *Segnode[v]
+	Segmenter Segmenter
 }
 
-type Trie struct {
-	RootNode   *Node
-	DeleteNode *Node
-}
-
-var delteTire *Trie
-
-func NewTrie(needs_delete bool, clean_deleated_nodes_time time.Duration) *Trie {
-	new_trie := &Trie{RootNode: NewNode()}
-	if needs_delete {
-		go new_trie.backgroundDelete(clean_deleated_nodes_time)
+func keysegmentfunc(key string, init int) (ret string, next int) {
+	if len(key) == 0 || init < 0 || init > len(key)-1 {
+		return "", -1
 	}
-	return new_trie
-}
 
-func NewNode() *Node {
-	node := &Node{}
-	for i := 0; i < 27; i++ {
-		node.Children[i] = nil
+	end := strings.IndexRune(key[init+1:], ' ')
+	if end == -1 {
+		return key[init:], -1
 	}
-	return node
+	return key[init : init+end+1], init + end + 2
 }
 
-func NewNodeValue(value string, has_ttl bool, ttl time.Duration) **NodeValues {
-	nv := &NodeValues{Value: value}
-	nv.Done = make(chan struct{})
-	if has_ttl {
-		go func() {
-			select {
-			case <-time.After(ttl):
-				close(nv.Done)
-				nv = nil
-			case <-nv.Done:
-				nv = nil
-			}
-		}()
+func NewSegNode[v any]() *Segnode[v] {
+	return &Segnode[v]{
+		Value:    []v{},
+		Children: swiss.NewMap[string, *Segnode[v]](42),
 	}
-	return &nv
 }
 
-func (t *Trie) Insert(key string, has_ttl bool, time_to_live int, is_auto_delete bool, values ...string) error {
-	current := t.RootNode
-	for i := 0; i < len(key); i++ {
-		index := key[i] - 'a'
-		if current.Children[index] == nil {
-			current.Children[index] = NewNode()
+func NewSegmap[v any](segmenter Segmenter) *Segmap[v] {
+	s := Segmap[v]{}
+	s.Node = NewSegNode[v]()
+	if segmenter == nil {
+		s.Segmenter = keysegmentfunc
+	} else {
+		s.Segmenter = segmenter
+	}
+
+	return &s
+}
+
+func (s *Segmap[v]) Put(key string, value ...v) int {
+	segnode := s.Node
+	for seg, i := s.Segmenter(key, 0); seg != ""; seg, i = s.Segmenter(key, i) {
+		segnode.Mu.Lock()
+		child, ok := segnode.Children.Get(seg)
+		if !ok {
+			child = NewSegNode[v]()
+			segnode.Children.Put(seg, child)
 		}
-		current = current.Children[index]
+		segnode.Mu.Unlock()
+		segnode = child
 	}
-	current.AutoDelete = true
-
-	for _, v := range values {
-		nv := NewNodeValue(v, has_ttl, time.Duration(time_to_live)*time.Second)
-		current.Values = append(current.Values, nv)
-	}
-
-	return nil
+	segnode.Value = append(segnode.Value, value...)
+	return len(segnode.Value)
 }
 
-func (n *Node) InsertKeyInPointer(key interface{}) (*Node, error) {
-	new_node := NewNode()
-	switch key := key.(type) {
-	case int:
-		n.Children[key] = new_node
-	case rune:
-		n.Children[key-'a'] = new_node
-	default:
-		return nil, fmt.Errorf("key Can Only be string or int")
+func (s *Segmap[v]) Get(key string) []v {
+	segnode := s.Node
+	for seg, i := s.Segmenter(key, 0); seg != ""; seg, i = s.Segmenter(key, i) {
+		segnode.Mu.RLock()
+		child, ok := segnode.Children.Get(seg)
+		if !ok {
+			segnode.Mu.RUnlock()
+			return nil
+		}
+		segnode.Mu.RUnlock()
+		segnode = child
 	}
-	return new_node, nil
+	return segnode.Value
 }
 
-func (t *Trie) InsertHereIfAnyChildHasValue(key string, has_ttl bool, time_to_live int, values ...string) error {
-	current_pointer := t.RootNode
-	for i := 0; i < len(key); i++ {
-		index := key[i] - 'a'
-		if current_pointer.Children[index] == nil {
-			new_pointer, err := current_pointer.InsertKeyInPointer(index)
-			if err != nil {
-				return err
-			}
-			current_pointer = new_pointer
-		} else {
-			current_pointer = current_pointer.Children[index]
-		}
-	}
+func (s *Segnode[v]) walk() []v {
+	if s != nil {
+		var result []v
+		s.Mu.RLock()
+		result = append(result, s.Value...)
+		s.Mu.RUnlock()
+		s.Children.Iter(func(k string, seg *Segnode[v]) (stop bool) {
+			seg.Mu.RLock()
+			result = append(result, seg.walk()...)
+			seg.Mu.RUnlock()
+			return false
+		})
 
-	should_transverse := false
-
-	for _, children := range current_pointer.Children {
-		if children != nil {
-			should_transverse = true
-			break
-		}
-	}
-
-	if should_transverse {
-		all_inner_value := t.traverse(current_pointer, key)
-		for _, v := range all_inner_value {
-			current_pointer.Values = append(current_pointer.Values, NewNodeValue(v, false, 0))
-		}
-
+		return result
 	}
 	return nil
 }
 
-func (t *Trie) Delete() {
+func (s *Segmap[v]) Transverse(key string) []v {
 
-}
-
-func (t *Trie) backgroundDelete(clean_deleated_nodes_time time.Duration) {
-	t.DeleteNode = NewNode()
-
-	time.Sleep(time.Duration(5))
-	t.backgroundDelete(clean_deleated_nodes_time)
-}
-
-func (t *Trie) traverse(current *Node, key string) []string {
-	var result []string
-	if current != nil {
-		auto_delete := current.AutoDelete
-
-		for _, v := range current.Values {
-			val := (*v)
-			if val != nil {
-
-				result = append(result, val.Value)
-
-				if auto_delete {
-					select {
-					case <-val.Done:
-					default:
-						close(val.Done)
-					}
-				}
-			}
+	segnode := s.Node
+	for seg, i := s.Segmenter(key, 0); seg != ""; seg, i = s.Segmenter(key, i) {
+		segnode.Mu.RLock()
+		child, ok := segnode.Children.Get(seg)
+		if !ok {
+			segnode.Mu.RUnlock()
+			return nil
 		}
-
-		for k, child := range current.Children {
-			if child != nil {
-				result = append(result, t.traverse(child, key+string(k))...)
-			}
-		}
-
-		if auto_delete {
-			current.Values = nil
-			t.Delete()
-		}
+		segnode.Mu.RUnlock()
+		segnode = child
 	}
-	return result
-}
+	return segnode.walk()
 
-func (t *Trie) SearchThrough(key string) []string {
-	current_pointer := t.RootNode
-	for i := 0; i < len(key); i++ {
-		index := key[i] - 'a'
-		if current_pointer == nil || current_pointer.Children[index] == nil {
-			return []string{}
-		}
-		current_pointer = current_pointer.Children[index]
-	}
-
-	return t.traverse(current_pointer, key)
 }
 
 func main() {
-	new_node := NewTrie(true, time.Duration(5))
-
-	new_node.Insert("hel", true, 2, true, "hel")
-	new_node.Insert("hello", true, 2, true, "hello")
-	new_node.Insert("hel", false, 2, true, "hel")
-	new_node.Insert("he", true, 2, true, "he")
-	new_node.Insert("h", true, 2, true, "h")
-	new_node.Insert("se", true, 2, true, "se")
-	fmt.Println(new_node.SearchThrough("hello"))
-	fmt.Println(new_node.SearchThrough("se"))
-	fmt.Println(new_node.SearchThrough("he"))
-	fmt.Println(new_node.SearchThrough("h"))
-	fmt.Println(new_node.SearchThrough("h"))
-
-	time.Sleep(3 * time.Second)
+	segmap := NewSegmap[string](nil)
+	segmap.Put("tuutf e f e", "Hello1")
+	segmap.Put("tuutf e f x", "Hello2")
+	segmap.Put("tuutf e f t", "Hello3")
+	fmt.Println(segmap.Get("tuutf e f e"))
+	fmt.Println(segmap.Get("tuutf e f"))
+	fmt.Println(segmap.Transverse("tuutf e f"))
 }
